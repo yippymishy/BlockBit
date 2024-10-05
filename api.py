99ce124db775d5613d0ad12c4ce0887bc86dc840
LIZARD-OFFICIAL-ALT <lizard.official.77+justanalt@gmail.com>
2024-10-05 20:56:02 +0200

    add a damn query route you strugl

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, abort, render_template
 from local_simple_database import LocalDictDatabase
-
+from json import loads
 # Setup database
 LDD = LocalDictDatabase(str_path_database_dir=".", default_value=None)
 
@@ -10,6 +10,8 @@ transactions_db = LDD["dict_transactions"]
 
 # Flask stuff
 app = Flask(__name__)
+
+
 
 # Main route
 @app.route('/')
@@ -23,6 +25,16 @@ def balance(username):
         return jsonify({'success': True, 'username': username, 'balance': balance})
     except KeyError:
         return jsonify({'success': False, 'message': 'User not found'})
+
+# goddamit yippy why did you not add this already
+# it goes thru the database and returns all the transactions the user was involved in.
+@app.route('/query/<username>')
+def query_user(username):
+    result = {}
+    for k,v in loads(str(transactions_db)).items():
+        if v["from"] == username or v["to"] == username:
+            result.update({k:v})
+    return result
 
 if __name__ == '__main__':
     app.run()
